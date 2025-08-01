748054ef29cc2975beb059ddff67d7ae66c7a14e
farahorfaly04 <farah.orfaly@gmail.com>
2025-08-01 17:45:49 +0200

    tets

--- test_led_strip.py
+++ test_led_strip.py
@@ -1,13 +1,13 @@
 from rpi_ws281x import PixelStrip, Color
-
+import time
 # LED configuration:
-LED_COUNT = 30         # Number of LED pixels
-LED_PIN = 13           # GPIO 13
+LED_COUNT =  62         # Number of LED pixels
+LED_PIN = 18           # GPIO 13
 LED_FREQ_HZ = 800000   # LED signal frequency
 LED_DMA = 10           # DMA channel
 LED_BRIGHTNESS = 255   # Max brightness
 LED_INVERT = False     # True if using NPN transistor level shift
-LED_CHANNEL = 1        # Channel 1 for GPIO 13
+LED_CHANNEL = 0        # Channel 1 for GPIO 13
 
 def setup_leds():
     strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT,
@@ -19,6 +19,7 @@ def simple_test(strip):
     for i in range(strip.numPixels()):
         strip.setPixelColor(i, Color(255, 0, 0))  # Red
     strip.show()
+    time.sleep(1)
 
 # Example usage:
 if __name__ == '__main__':
