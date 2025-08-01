0f47d9aa2ca8a608fef1e8c8aee87665259e83c0
farah orfaly <farahorfaly04@users.noreply.github.com>
2025-08-01 18:41:55 +0200

    test

--- test_led_strip.py
+++ test_led_strip.py
@@ -1,21 +1,36 @@
-from rpi_ws281x import Adafruit_NeoPixel, Color
+import argparse
 import time
+from rpi_ws281x import Adafruit_NeoPixel, Color
 
 # LED configuration
-STRIP1_COUNT = 62
-STRIP1_PIN = 18  # GPIO 18
+STRIP1_COUNT = 2
+STRIP1_PIN = 18
 LED_FREQ_HZ = 800000
 LED_DMA = 10
 LED_BRIGHTNESS = 100
 LED_INVERT = False
 LED_CHANNEL = 0
 
-# Initialize
-strip1 = Adafruit_NeoPixel(STRIP1_COUNT, STRIP1_PIN, LED_FREQ_HZ, LED_DMA,
-                           LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-strip1.begin()
+def run_test(clear=False):
+    strip1 = Adafruit_NeoPixel(STRIP1_COUNT, STRIP1_PIN, LED_FREQ_HZ, LED_DMA,
+                                LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    strip1.begin()
 
-# Test: fill with green
-for i in range(strip1.numPixels()):
-    strip1.setPixelColor(i, Color(0, 255, 0))  # Green
-strip1.show()
+    # Light up green
+    for i in range(strip1.numPixels()):
+        strip1.setPixelColor(i, Color(0, 255, 0))
+    strip1.show()
+    time.sleep(1)
+
+    # Clear if requested
+    if clear:
+        for i in range(strip1.numPixels()):
+            strip1.setPixelColor(i, 0)
+        strip1.show()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--clear', action='store_true', help='Clear LEDs after lighting')
+    args = parser.parse_args()
+
+    run_test(clear=args.clear)
